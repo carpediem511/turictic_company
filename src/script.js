9e0140e6f63d12bdc5f437ae4593ba5909750e23
carpediem511 <vampire.0514@gmail.com>
2022-11-12 14:30:19 +0300

    сделан третий блок

---
+++ src/script.js
@@ -0,0 +1,63 @@
+/* let places = [
+    {
+    title: "водопады Бахо",
+    addToFavorites: "./icon-like.png",
+    price: "100$",
+    duration: "5 часов",
+    daysOfWeek: "понедельник",
+    more: "./icon-right.png",
+    image: "./image-baho.png",
+    },
+
+    {
+        title: "пляж Доклет",
+        addToFavorites: "./icon-like.png",
+        price: "100$",
+        duration: "5 часов",
+        daysOfWeek: "пятница",
+        more: "./icon-right.png",
+        image: "./image-beech.jpg",
+        },
+
+        {
+            title: "Далат",
+            addToFavorites: "./icon-like.png",
+            price: "250$",
+            duration: "2 дня",
+            daysOfWeek: "суббота",
+            more: "./icon-right.png",
+            image: "./image-dalat.jpg",
+            },
+
+            {
+                title: "Vinperl - парк развлечений",
+                addToFavorites: "./icon-like.png",
+                price: "150$",
+                duration: "целый день",
+                daysOfWeek: "воскресенье",
+                more: "./icon-right.png",
+                image: "./vinperl.jpg",
+                },
+
+                {
+                    title: "Hotel <Crazy House>",
+                    addToFavorites: "./icon-like.png",
+                    price: "110$",
+                    duration: "4 часа",
+                    daysOfWeek: "вторник",
+                    more: "./icon-right.png",
+                    image: "./image-hotel.png",
+                    },
+
+                    {
+                        title: "Гастрономический тур",
+                        addToFavorites: "./icon-like.png",
+                        price: "50$",
+                        duration: "3 часа",
+                        daysOfWeek: "среда",
+                        more: "./icon-right.png",
+                        image: "./image-food.png",
+                        },
+]
+*/
+
