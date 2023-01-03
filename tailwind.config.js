140664be3f56fae17ace8a03f7609aa20d22b910
carpediem511 <vampire.0514@gmail.com>
2023-01-03 12:25:19 +0300

    создание контрольных точек

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
       colors: {
         yellow: "#FFC700",
       },
+      screens: {
+        'laptope-L': {'max': '1440px'},
+        'laptope-M': {'max': '1280px'},
+        'laptope-S': {'max': '1024px'},
+        'tablet': {'max': '768px'},
+        'mobile-L': {'max': '600px'},
+        'mobile-M': {'max': '425px'},
+        'mobile-S': {'max': '375px'},
+        'mobile-XS': {'max': '320px'},
+      },
     },
   },
   plugins: [],
