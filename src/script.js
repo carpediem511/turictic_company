739892d4f5dd21c7b5d5ed76c80ba162a41ac01d
carpediem511 <vampire.0514@gmail.com>
2023-01-10 15:45:53 +0300

    почти готово

--- src/script.js
+++ src/script.js
@@ -61,33 +61,28 @@
 ]
 */
 
-
 let isOpen = false
 
+let menuImg = document.getElementById("iconMenu")
+let openMobileMenu = document.getElementById("mobileMenu")
+let buttonOpenMenu = document.getElementById("openMenuButton")
 
+buttonOpenMenu.addEventListener("click", openMenu)
 
-
-   let menuImg = document.getElementById("iconMenu")
-   let openMobileMenu = document.getElementById("mobileMenu")
-   let buttonOpenMenu = document.getElementById("openMenuButton")
-
-   buttonOpenMenu.addEventListener("click", openMenu)
-
-   function openMenu() {
- 
-   if (buttonOpenMenu) { // если у нас нажали на элемент 
-		if (isOpen) { // если он уже был открыт, значит, нажали еще раз - закрываем
-			// поменять иконку
+function openMenu() {
+  if (buttonOpenMenu) {
+    // если у нас нажали на элемент
+    if (isOpen) {
+      // если он уже был открыт, значит, нажали еще раз - закрываем
+      // поменять иконку
       openMobileMenu.style.display = "none"
-            menuImg.src = "./src/images/icon-menu.png"
-			isOpen = false // установить закрытым
-   
-		} else {
-			// поменять иконку	
+      menuImg.src = "./src/images/icon-menu.png"
+      isOpen = false // установить закрытым
+    } else {
+      // поменять иконку
       openMobileMenu.style.display = "flex"
-         menuImg.src = "./src/images/icon-menu2.png"
-			isOpen = true // установить открытым
-        
-		}
- 	}
+      menuImg.src = "./src/images/icon-menu2.png"
+      isOpen = true // установить открытым
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,21 @@ module.exports = {
         yellow: "#FFC700",
       },
       screens: {
-        'mini': '375px',
+        mobile: "320px",
 
-        'sm': '640px',
+        mini: "375px",
+
+        sm: "640px",
         // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
+
+        md: "768px",
         // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
+
+        lg: "1024px",
         // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
+
+        xl: "1280px",
         // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
       },
       fontSize: {
         min: ["8px", "1.1"],
